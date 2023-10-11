2b80ef44e1bd3e9256b776b17d58ad96913ed548
mwnCoding <MaximilianoNaddeo@gmail.com>
2023-10-11 18:35:42 +0200

    idk

--- src/pages/GameDetails.jsx
+++ src/pages/GameDetails.jsx
@@ -1,104 +1,123 @@
-import { Link, NavLink, Route, Routes, useParams, useNavigate } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import '../App.css'
-import metacritic from "../assets/metacritic.png"
-import Navbar from '../components/Navbar'
-const apiKey = "03d5981c38e0462b823b8533abbe6af3"
+import {
+  Link,
+  NavLink,
+  Route,
+  Routes,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
+import { useEffect, useState } from "react";
+import "../App.css";
+import metacritic from "../assets/metacritic.png";
+import Navbar from "../components/Navbar";
+const apiKey = "03d5981c38e0462b823b8533abbe6af3";
 function GameDetails() {
-    const [game, setGame] = useState([])
-    const { id, gameId } = useParams()
-    const navigate = useNavigate()
-    const fetchAllGames = async () => {
-        const response = await fetch(`https://api.rawg.io/api/games/${gameId}?key=${apiKey}`, {
-            method: 'GET',
-        })
-        console.log(response)
-        if (response.status === 200) {
-            const thisGame = await response.json()
-            setGame(thisGame)
-            console.log(thisGame)
-        }
+  const [game, setGame] = useState([]);
+  const { id, gameId } = useParams();
+  const navigate = useNavigate();
+  const fetchAllGames = async () => {
+    const response = await fetch(
+      `https://api.rawg.io/api/games/${gameId}?key=${apiKey}`,
+      {
+        method: "GET",
+      }
+    );
+    console.log(response);
+    if (response.status === 200) {
+      const thisGame = await response.json();
+      setGame(thisGame);
+      console.log(thisGame);
     }
-    useEffect(() => {
-        try { fetchAllGames() }
-        catch { (error) => { console.log(error) } }
-    }, [])
-
-
-
-    const addGamePlaying = async () => {
-        const payload = {
-            userId: 1,
-            id: game.id,
-            title: game.name,
-            image: game.background_image,
-            notes: [],
-            milestones: []
-        }
-        try {
-            const response = await fetch(`https://gamejournal-backend-2023.adaptable.app/games`, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: {
-                    'Content-type': 'application/json',
-                },
-            })
-            if (response.ok) {
-                const currentGame = await response.json()
-                console.log(currentGame)
-                navigate(`/games-playing/${id}`)
-            }
-        }
-        catch (error) {
-            console.log(error)
-        }
+  };
+  useEffect(() => {
+    try {
+      fetchAllGames();
+    } catch {
+      (error) => {
+        console.log(error);
+      };
     }
+  }, []);
 
-    const addGameNotPlaying = async () => {
-        const payload = {
-            userId: 1,
-            id: game.id,
-            title: game.name,
-            image: game.background_image,
+  const addGamePlaying = async () => {
+    const payload = {
+      userId: id,
+      id: game.id,
+      title: game.name,
+      image: game.background_image,
+      notes: [],
+      milestones: [],
+    };
+    try {
+      const response = await fetch(
+        `https://gamejournal-backend-2023.adaptable.app/games`,
+        {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-type": "application/json",
+          },
         }
-        try {
-            const response = await fetch(`https://gamejournal-backend-2023.adaptable.app/gamesNotPlaying`, {
-                method: "POST",
-                body: JSON.stringify(payload),
-                headers: {
-                    'Content-type': 'application/json',
-                },
-            })
-            if (response.ok) {
-                const currentGame = await response.json()
-                console.log(currentGame)
-                navigate(`/game-list/${id}`)
-
-            }
-        }
-        catch (error) {
-            console.log(error)
-        }
+      );
+      if (response.ok) {
+        const currentGame = await response.json();
+        console.log(currentGame);
+        navigate(`/games-playing/${id}`);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
+  const addGameNotPlaying = async () => {
+    const payload = {
+      userId: id,
+      id: game.id,
+      title: game.name,
+      image: game.background_image,
+    };
+    try {
+      const response = await fetch(
+        `https://gamejournal-backend-2023.adaptable.app/gamesNotPlaying`,
+        {
+          method: "POST",
+          body: JSON.stringify(payload),
+          headers: {
+            "Content-type": "application/json",
+          },
+        }
+      );
+      if (response.ok) {
+        const currentGame = await response.json();
+        console.log(currentGame);
+        navigate(`/game-list/${id}`);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-    return (
-
-        (
-            <>
-                <Navbar id={id} />
-                <div className='container'>
-                    <img src={game.background_image} style={{ height: "200px" }} />
-                    <p className='gameName'>{game.name}</p>
-                    <div>About: {game.description_raw}</div>
-                    <p className='score'><img className='metacritic' src={metacritic} />Metacritic score: {game.metacritic}</p>
-                    <p>Average playtime: {game.playtime} hrs</p>
-                    <button className='btnAdd' onClick={addGamePlaying}>Add to my games</button>
-                    <button className='btnNotInterested' onClick={addGameNotPlaying}>I'm not interested</button>
-                </div>
-            </>
-        ))
-
+  return (
+    <>
+      <Navbar id={id} />
+      <div className="container">
+        <img src={game.background_image} style={{ height: "200px" }} />
+        <p className="gameName">{game.name}</p>
+        <div>About: {game.description_raw}</div>
+        <p className="score">
+          <img className="metacritic" src={metacritic} />
+          Metacritic score: {game.metacritic}
+        </p>
+        <p>Average playtime: {game.playtime} hrs</p>
+        <button className="btnAdd" onClick={addGamePlaying}>
+          Add to my games
+        </button>
+        <button className="btnNotInterested" onClick={addGameNotPlaying}>
+          I'm not interested
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default GameDetails;
--- src/pages/GamePlayingDetails.jsx
+++ src/pages/GamePlayingDetails.jsx
@@ -1,123 +1,146 @@
-import { Link, NavLink, Route, Routes, useParams } from 'react-router-dom'
-import { useEffect, useState } from 'react'
-import '../App.css'
-import Navbar from '../components/Navbar'
+import { Link, NavLink, Route, Routes, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import "../App.css";
+import Navbar from "../components/Navbar";
 function GamesPlayingDetails() {
-    const [milestones, setMilestones] = useState([])
-    const [newMilestone, setNewMilestone] = useState([])
-    const [notes, setNotes] = useState([])
-    const [game, setGame] = useState({})
-    const [newNote, setNewNote] = useState("")
-    const { id, gameId } = useParams()
-    const fetchThisGame = async () => {
-        const response = await fetch(`https://gamejournal-backend-2023.adaptable.app/games/${gameId}`, {
-            method: 'GET',
-        })
-        console.log(response)
-        if (response.status === 200) {
-            const thisGame = await response.json()
-            setGame(thisGame)
-            setNotes(thisGame.notes)
-            setMilestones(thisGame.milestones)
-            console.log(thisGame)
-        }
+  const [milestones, setMilestones] = useState([]);
+  const [newMilestone, setNewMilestone] = useState([]);
+  const [notes, setNotes] = useState([]);
+  const [game, setGame] = useState({});
+  const [newNote, setNewNote] = useState("");
+  const { id, gameId } = useParams();
+  const fetchThisGame = async () => {
+    const response = await fetch(
+      `https://gamejournal-backend-2023.adaptable.app/games/${gameId}`,
+      {
+        method: "GET",
+      }
+    );
+    console.log(response);
+    if (response.status === 200) {
+      const thisGame = await response.json();
+      setGame(thisGame);
+      setNotes(thisGame.notes);
+      setMilestones(thisGame.milestones);
+      console.log(thisGame);
     }
+  };
 
-    const addNote = async event => {
-        event.preventDefault()
-        const updatedNotes = { notes: [...notes, newNote] }
-        console.log(updatedNotes)
-        try {
-            const response = await fetch(
-                `https://gamejournal-backend-2023.adaptable.app/games/${gameId}`,
-                {
-                    method: 'PATCH',
-                    body: JSON.stringify(updatedNotes),
-                    headers: {
-                        'Content-type': 'application/json',
-                    },
-                }
-            )
-            if (response.ok) {
-                const currentNotes = await response.json()
-                console.log(currentNotes)
-                window.location.reload(false);
-            }
-        } catch (error) {
-            console.log(error)
+  const addNote = async (event) => {
+    event.preventDefault();
+    const updatedNotes = { notes: [...notes, newNote] };
+    console.log(updatedNotes);
+    try {
+      const response = await fetch(
+        `https://gamejournal-backend-2023.adaptable.app/games/${gameId}`,
+        {
+          method: "PATCH",
+          body: JSON.stringify(updatedNotes),
+          headers: {
+            "Content-type": "application/json",
+          },
         }
+      );
+      if (response.ok) {
+        const currentNotes = await response.json();
+        console.log(currentNotes);
+        window.location.reload(false);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const addMilestone = async event => {
-        event.preventDefault()
-        const updatedMilestones = { milestones: [...milestones, newMilestone] }
-        console.log(updatedMilestones)
-        try {
-            const response = await fetch(
-                `https://gamejournal-backend-2023.adaptable.app/games/${gameId}`,
-                {
-                    method: 'PATCH',
-                    body: JSON.stringify(updatedMilestones),
-                    headers: {
-                        'Content-type': 'application/json',
-                    },
-                }
-            )
-            if (response.ok) {
-                const currentMilestones = await response.json()
-                console.log(currentMilestones)
-                window.location.reload(false);
-            }
-        } catch (error) {
-            console.log(error)
+  const addMilestone = async (event) => {
+    event.preventDefault();
+    const updatedMilestones = { milestones: [...milestones, newMilestone] };
+    console.log(updatedMilestones);
+    try {
+      const response = await fetch(
+        `https://gamejournal-backend-2023.adaptable.app/games/${gameId}`,
+        {
+          method: "PATCH",
+          body: JSON.stringify(updatedMilestones),
+          headers: {
+            "Content-type": "application/json",
+          },
         }
+      );
+      if (response.ok) {
+        const currentMilestones = await response.json();
+        console.log(currentMilestones);
+        window.location.reload(false);
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    useEffect(() => {
-        try { fetchThisGame() }
-        catch { (error) => { console.log(error) } }
-    }, [])
-    useEffect(() => {
-        console.log(game)
-    }, [game])
+  useEffect(() => {
+    try {
+      fetchThisGame();
+    } catch {
+      (error) => {
+        console.log(error);
+      };
+    }
+  }, []);
+  useEffect(() => {
+    console.log(game);
+  }, [game]);
 
-    return (
-        <>
-            <Navbar id={id} />
-            <div className='container'>
-                <img src={game.image} style={{ height: "200px" }} />
-                <p className='gameName'>{game.title}</p>
-                <p>Milestones:</p>
-                {milestones.map(milestone => (
-                    <div className='milestone'>
-                        <input type="checkbox" id="cbox" value="checkbox" />
-                        <p>{milestone}</p>
-                    </div>
-                )
-                )}
-                <form onSubmit={addMilestone}>
-                    <label htmlFor="newMilestone">
-                        New milestone
-                        <input name="newMilestone" value={newMilestone} type="text" onChange={event => setNewMilestone(event.target.value)} required />
-                    </label>
-                    <button className='addButtonGames' type="submit">Add</button>
-                </form>
-                <p>Notes:</p>
-                {notes.map(note => (
-                    <div>
-                        <p className='note'>{note}</p>
-                    </div>
-                )
-                )}
-                <form onSubmit={addNote}>
-                    <label htmlFor="newNote">
-                        New note
-                        <input name="newNote" value={newNote} type="text" onChange={event => setNewNote(event.target.value)} required />
-                    </label>
-                    <button className='addButtonGames' type="submit">Add</button>
-                </form>
-            </div>
-        </>)
+  return (
+    <>
+      <Navbar id={id} />
+      <div className="container">
+        <img src={game.image} style={{ height: "200px" }} />
+        <p className="gameName">{game.title}</p>
+        <p>Milestones:</p>
+        {milestones.map((milestone) => (
+          <div className="milestone">
+            <input type="checkbox" id="cbox" value="checkbox" />
+            <p>{milestone}</p>
+          </div>
+        ))}
+        <form onSubmit={addMilestone}>
+          <label htmlFor="newMilestone">
+            New milestone
+            <input
+              name="newMilestone"
+              value={newMilestone}
+              type="text"
+              onChange={(event) => setNewMilestone(event.target.value)}
+              required
+            />
+          </label>
+          <button className="addButtonGames" type="submit">
+            Add
+          </button>
+        </form>
+        <p>Notes:</p>
+        {notes.map((note) => (
+          <div>
+            <p className="note">{note}</p>
+          </div>
+        ))}
+        <form onSubmit={addNote}>
+          <label htmlFor="newNote">
+            New note
+            <input
+              name="newNote"
+              value={newNote}
+              type="text"
+              onChange={(event) => setNewNote(event.target.value)}
+              required
+            />
+          </label>
+          <button className="addButtonGames" type="submit">
+            Add
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default GamesPlayingDetails;
